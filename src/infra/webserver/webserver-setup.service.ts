e2f7079c55e098e3959aae0f4b921b1895511831
Nikita Shevelev <nikita.shevelev@imem.app>
2024-07-25 19:08:44 +0300

    Add dotenv dependency

--- src/infra/webserver/webserver-setup.service.ts
+++ src/infra/webserver/webserver-setup.service.ts
@@ -1,17 +1,18 @@
 import { INestApplication, Injectable, Logger } from '@nestjs/common';
 import { WebserverConfig } from '@infra/webserver/webserver.config';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 @Injectable()
 export class WebserverSetupService {
-  constructor(
-    private readonly config: WebserverConfig,
-  ) {
-  }
+  private readonly appName = 'chepoauto-bot';
+
+  constructor(private readonly config: WebserverConfig) {}
 
   public async setup(app: INestApplication): Promise<void> {
     await app.listen(this.config.port);
 
-    const msg = `Serving service on ${this.config.publicUrl}`;
+    const msg = `Serving ${this.appName} on ${this.config.port} port`;
     new Logger('Webserver').log(msg);
   }
 }
